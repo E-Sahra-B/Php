8524807d2e8cef9cadf4735a326990bdbbb8b05f
E-Sahra-B <bendenizsahra@gmail.com>
2022-11-10 21:02:00 +0300

    30Ders
    30Ders

---
+++ 30Ders.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="tr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHPMyAdmin Veritabani Yönetim Sistemi Kullanimi</title>
+</head>
+
+<body>
+    <h3>PHPMyAdmin Veritabani Yönetim Sistemi Kullanimi</h3>
+
+    <hr>
+    <?php
+    ?>
+</body>
+
+</html>
